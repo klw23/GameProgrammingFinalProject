30a702d3ed9721dd76ad73366da48d5ea3a133ee
Karen Zhu <68167276+zkaren01@users.noreply.github.com>
2024-03-12 21:50:24 -0400

    Implementation of Character Movement  (#2)
    * File Directory Organization
    
    Moved files around!
    
    * Implemented New Player Movement
    
    Player rotation follows mouse. WASD to move the character.

---
+++ Assets/Scripts/PlayerAndRodController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAndRodController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float rotationSpeed = 700;
+    CharacterController controller;
+    Vector3 input, moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (!PoleBehavior.isReeledIn)
+        {
+            // do not let player move
+        }
+        else
+        {
+            moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
+            moveDirection.Normalize();
+
+            controller.Move(moveDirection * moveSpeed * Time.deltaTime); //allows us to move the character based on keyboard input
+
+        }
+    }
+}
