918bf6371feaef3006a32d3302d57323b38e0917
Karen Zhu <68167276+zkaren01@users.noreply.github.com>
2024-03-18 22:21:29 -0400

    Add Walking Animation and Removed Fish Cast animation

--- Assets/Scripts/PlayerAndRodController.cs
+++ Assets/Scripts/PlayerAndRodController.cs
@@ -6,6 +6,8 @@ public class PlayerAndRodController : MonoBehaviour
 {
     public float moveSpeed = 5;
     public float rotationSpeed = 700;
+    public Animator anim;
+
     CharacterController controller;
     Vector3 moveDirection;
     // Start is called before the first frame update
@@ -17,20 +19,25 @@ public class PlayerAndRodController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
+        Debug.Log("Horizontal" + Input.GetAxisRaw("Horizontal") + "Vertical" + Input.GetAxisRaw("Vertical"));
         if (!PoleBehavior.isReeledIn)
         {
             // do not let player move
         }
-        else
+        else if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            anim.SetInteger("FishingAnim", 1);
             moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
             moveDirection.Normalize();
 
             controller.Move(moveDirection * moveSpeed * Time.deltaTime); //allows us to move the character based on keyboard input
 
+        } else
+        {
+            anim.SetInteger("FishingAnim", 0);
         }
     }
 }
--- Assets/Scripts/PoleBehavior.cs
+++ Assets/Scripts/PoleBehavior.cs
@@ -9,7 +9,6 @@ public class PoleBehavior : MonoBehaviour
     public GameObject bob;
     public GameObject player;
     public float projectileSpeed = 10f;
-    public Animator anim;
 
     LineRenderer lr;
     Rigidbody bobRB;
@@ -32,19 +31,14 @@ public class PoleBehavior : MonoBehaviour
 
         if (Input.GetButtonDown("Fire1") && isReeledIn)
         {
-            anim.SetInteger("FishingAnim", 1);
-            isReeledIn = false;
-            var animDuration = anim.GetCurrentAnimatorStateInfo(0).length - 1.2f;
-            Invoke("castBob", animDuration);
+            castBob();
         } 
         else if (Input.GetButtonDown("Fire1") && !isReeledIn)
         {
-            anim.SetInteger("FishingAnim", 0);
             reelBob();
         } 
         else if (isReeledIn)
         {
-            anim.SetInteger("FishingAnim", 0);
             UpdateBobStartingPos();
         }
     }
@@ -59,6 +53,7 @@ public class PoleBehavior : MonoBehaviour
         bobStartingPos = bob.transform.position;
         bobRB.useGravity = true;
         bobRB.AddForce(player.transform.forward * projectileSpeed, ForceMode.VelocityChange);
+        isReeledIn = false;
     }
 
     void reelBob()
