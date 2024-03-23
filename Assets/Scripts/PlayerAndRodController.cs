60c9b19bee7390e01529b2b4e5f63a66851b606a
Kaitlyn Wong <kaitlyn23wong@gmail.com>
2024-03-22 22:28:03 -0400

    island version

--- Assets/Scripts/PlayerAndRodController.cs
+++ Assets/Scripts/PlayerAndRodController.cs
@@ -4,44 +4,73 @@ using UnityEngine;
 
 public class PlayerAndRodController : MonoBehaviour
 {
+
+    // public variables
     public static bool isWalking = false;
     public float moveSpeed = 5;
     public float rotationSpeed = 700;
     public Animator anim;
 
+    public float minX = 147f;
+    public float maxX = 419f;
+    public float minZ = -534f;
+    public float maxZ = 810f;
+    public GameObject islandCenter;
+
+
+    // private variables
     CharacterController controller;
     Vector3 moveDirection;
-    // Start is called before the first frame update
+
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         Debug.Log("Horizontal" + Input.GetAxisRaw("Horizontal") + "Vertical" + Input.GetAxisRaw("Vertical"));
+        print("touching barrier " + PlayerBarrier.touchingCollider);
+
         if (!PoleBehavior.isReeledIn)
         {
-            // do not let player move
+            // Do not let player move
             isWalking = false;
+            return; // Early return to prevent further execution in this case
         }
-        else if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 intendedDirection = new Vector3(moveHorizontal, 0, moveVertical).normalized;
+        Vector3 currentPosition = transform.position;
+        Vector3 nextPosition = currentPosition + intendedDirection * moveSpeed * Time.deltaTime;
+        Vector3 centerPosition = islandCenter.transform.position;
+
+        // Calculate distances
+        float currentDistance = Vector3.Distance(currentPosition, centerPosition);
+        float nextDistance = Vector3.Distance(nextPosition, centerPosition);
+
+        // Determine if moving closer to the center
+        bool movingTowardsCenter = nextDistance < currentDistance;
+
+
+        print("center " + movingTowardsCenter);
+        if (movingTowardsCenter || !PlayerBarrier.touchingCollider)
         {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-
             anim.SetInteger("FishingAnim", 1);
-            moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
-            moveDirection.Normalize();
-
-            controller.Move(moveDirection * moveSpeed * Time.deltaTime); //allows us to move the character based on keyboard input
-
+            moveDirection = intendedDirection;
+            controller.Move(moveDirection * moveSpeed * Time.deltaTime); // Allows us to move the character based on keyboard input
             isWalking = true;
-        } else
+        }
+        else
         {
             anim.SetInteger("FishingAnim", 0);
             isWalking = false;
         }
     }
+
+
 }
