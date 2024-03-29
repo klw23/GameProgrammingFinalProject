9269d25e894cb75118cf866417066adbda88e073
Karen Zhu <68167276+zkaren01@users.noreply.github.com>
2024-03-29 00:54:58 -0400

    Implement Jumping  (#12)
    * pull from main
    
    * Fix Pole Behavior and Bob Behavior to land on Water
    
    * Inverted movement direction to adjust to local forward
    
    * Implement Jump
    
    * Add Jumping Animation

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public Transform player; // The player's transform
-    public Vector3 offset; // Offset from the player
+    public Transform player;
+    public Transform tipOfFishingRod;
+    public float Xoffset;
+    public float Yoffset;
+    public float Zoffset;
     public float smoothSpeed = 0.125f; // How smoothly the camera follows the player
 
     void Update()
     {
-        Vector3 desiredPosition = player.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        
+        //Vector3 desiredPosition = player.position + offset;
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        //transform.position = smoothedPosition;
 
-        transform.LookAt(player); // Camera always looks at the player
+        Vector3 cameraPos = player.position;
+
+        cameraPos.x = player.position.x + Xoffset;
+        cameraPos.y = player.position.y + Yoffset;
+        cameraPos.z = player.position.z + Zoffset;
+         
+        transform.LookAt(tipOfFishingRod); // Camera always looks at the player
     }
 }
--- Assets/Scripts/PlayerAndRodController.cs
+++ Assets/Scripts/PlayerAndRodController.cs
@@ -9,48 +9,67 @@ public class PlayerAndRodController : MonoBehaviour
     public static bool isWalking = false;
     public float moveSpeed = 5;
     public float rotationSpeed = 700;
+    public float jumpHeight = 9.81f;
+    public float gravity = 9.81f;
     public Animator anim;
-
- 
-
 
     // private variables
     CharacterController controller;
-    Vector3 moveDirection;
-
+    Vector3 input, moveDirection;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
     }
 
-
     void Update()
     {
-        Debug.Log("Horizontal" + Input.GetAxisRaw("Horizontal") + "Vertical" + Input.GetAxisRaw("Vertical"));
-        if (!PoleBehavior.isReeledIn)
+        if (PoleBehavior.isReeledIn)
         {
-            // do not let player move
-            isWalking = false;
-        }
-        else if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-        {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
+            if (controller.isGrounded)
+            {
+                if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+                {
+                    anim.SetInteger("MichelleMovement", 1);
+                    onGroundMove();
+                    isWalking = true;
+                } 
+                else if (Input.GetButton("Jump"))
+                {
+                    anim.SetInteger("MichelleMovement", 2);
+                    moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+                    isWalking = false;
+                }
+                else
+                {
+                    anim.SetInteger("MichelleMovement", 0);
+                    moveDirection = Vector3.zero;
+                    isWalking = false;
+                }
+            } 
+            else
+            {
+                // midair 
+                input.y = moveDirection.y;
+                moveDirection = Vector3.Lerp(moveDirection, input, Time.deltaTime);
+                isWalking = false;
+            }
 
-            moveDirection = new Vector3(-moveHorizontal, 0, -moveVertical);
-            moveDirection.Normalize();
-
-            controller.Move(moveDirection * moveSpeed * Time.deltaTime); //allows us to move the character based on keyboard input
-            anim.SetInteger("MichelleMovement", 1);
-
-            isWalking = true;
-        }
-        else
-        {
-            anim.SetInteger("MichelleMovement", 0);
-            isWalking = false;
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
         }
     }
 
+    void onGroundMove()
+    {
+        print("hit");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(-moveHorizontal, 0, -moveVertical);
+        moveDirection.Normalize();
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime); //allows us to move the character based on keyboard input
+
+    }
 }
