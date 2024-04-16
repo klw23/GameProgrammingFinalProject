a53e267b0e0c73e937dc3b4cee9dc9c5d34b7540
krystalqiao <krystalqiao@gmail.com>
2024-04-16 11:55:22 -0400

    new loot for level3

--- Assets/Scripts/LootBehavior.cs
+++ Assets/Scripts/LootBehavior.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LootBehavior : MonoBehaviour
 {
-    public int timeAmount = 5;
+    public int timeAmount;
     public AudioClip collectLootSFX;
+
     public LootSpawner lootSpawner;
 
-    public GameObject player;
+    private int maxSharksSpawned = 2;
 
+    private int sharkSpawned = 0;
+    public GameObject sharkPrefab;
+    private GameObject sharkParent;
+    GameObject[] spawnPoints;
     void Start()
     {
         
@@ -23,17 +29,50 @@ public class LootBehavior : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player") && gameObject.CompareTag("Loot"))
+        if(other.CompareTag("Player") && gameObject.CompareTag("Hourglass"))
         {
             gameObject.SetActive(false);
             AudioSource.PlayClipAtPoint(collectLootSFX, transform.position);
 
             var levelManager = FindObjectOfType<LevelManagerBehavior>();
+
+            // loot gives 10 seconds if level is 3
+            if (SceneManager.GetActiveScene().name == "3 - LevelThreeScene1")
+            {
+                timeAmount = 10;
+            }
+            else 
+            {
+                timeAmount = 5;
+            }
+
             levelManager.IncreaseTime(timeAmount);
             levelManager.ShowAddedTime(timeAmount);
 
-            lootSpawner.LootCollected();
+            lootSpawner.LootCollected("Hourglass");
             Destroy(gameObject, 0.5f);
+        }
+
+        else if (other.CompareTag("Player") && gameObject.CompareTag("Star"))
+        {
+            gameObject.SetActive(false);
+            AudioSource.PlayClipAtPoint(collectLootSFX, transform.position);
+    
+            if (sharkSpawned < maxSharksSpawned)
+            {
+                int randomSpawnPoint = Random.Range(0, spawnPoints.Length - 1);
+                Vector3 spawnPosition = spawnPoints[randomSpawnPoint].transform.position;
+
+                // Instantiate fish and set its parent
+                GameObject newShark = Instantiate(sharkPrefab, spawnPosition, Quaternion.identity);
+                if (sharkParent != null)
+                {
+                    newShark.transform.SetParent(sharkParent.transform);
+                }
+
+                sharkSpawned++;
+            }
+
         }
     }
 }
